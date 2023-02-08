2d61a1a57a5ba179d289f4027ca67e81e2346631
Victor Lobo <victorlobo92@gmail.com>
2023-02-08 16:59:47 -0300

    Class: 3

--- src/components/layouts/Layouts.js
+++ src/components/layouts/Layouts.js
@@ -1,5 +1,6 @@
 import Container from './Container'
 import Footer from './Footer'
+import LinkButton from './LinkButton'
 import Navbar from './Navbar'
 
-export { Container, Footer, Navbar }
+export { Container, LinkButton, Footer, Navbar }
