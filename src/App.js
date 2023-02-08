bda4e06c64ddddf9e087d2b580564a5e46ef924b
Victor Lobo <victorlobo92@gmail.com>
2023-02-08 16:06:10 -0300

    Class: 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import * as page from './components/pages/Pages'
+import Container from './components/layouts/Container'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/new-project">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<page.Home />} />
+          <Route exact path="/company" element={<page.Company />} />
+          <Route exact path="/contact" element={<page.Contact />} />
+          <Route exact path="/new-project" element={<page.NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Pages.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+import Company from './Company'
+import Contact from './Contact'
+import NewProject from './NewProject'
+
+export { Home, Company, Contact, NewProject }
