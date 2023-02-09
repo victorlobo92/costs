b1a07412e4a19c0320955f1e39934352941f72d2
Victor Lobo <victorlobo92@gmail.com>
2023-02-09 15:46:19 -0300

    Class: 6

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,8 +4,18 @@ export default function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select id={name} name={name} onChange={handleOnChange} value={value}>
-        <option disabled>Selecione uma opção</option>
+      <select
+        id={name}
+        name={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 export default function NewProject() {
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json)
+      .then((data) => {
+        console.log(data);
+        navigate("/projects", { message: "Projeto criado com sucesso!" });
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className={styles.new_project_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar projeto" />
+      <ProjectForm btnText="Criar projeto" handleSubmit={createPost} />
     </div>
   );
 }
