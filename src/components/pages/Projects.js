d95c9956c8021c480e4b05e38337ed9b6c07ce8a
Victor Lobo <victorlobo92@gmail.com>
2023-02-09 17:37:45 -0300

    Class: 7

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,19 @@
+import { useLocation } from "react-router-dom";
+
+import Message from "../layouts/Message";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  const location = useLocation();
+  let message = "";
+
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message msg={message} type="success" />}
+    </div>
+  );
 }
